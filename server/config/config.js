8fb6d161a215125eaca93720192a268fe6f4eb1c
Shawn Phua <shawn2798@icloud.com>
2021-02-18 19:36:55 +0800

    initial set up

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+module.exports = {
+  "development": {
+    "username": process.env.PG_USER,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DB,
+    "host": process.env.PG_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.PG_USER,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DB,
+    "host": process.env.PG_HOST,
+    "dialect": "postgres"
+  }
+}
