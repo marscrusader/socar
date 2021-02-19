771853a2feb210f76e97073af7ecd70a79e46afd
Shawn <52193681+shawnlah@users.noreply.github.com>
2021-02-19 12:48:30 +0000

    refactor check iso string

--- server/utils/checkIsoString.js
+++ server/utils/checkIsoString.js
@@ -1,6 +1,2 @@
-export const isIsoString = (isoStr) => {
-  // Checks to see if its default JS iso date YYYY-MM-DDTHH:MN:SS.MSSZ
-  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(isoStr)) return false
-  const date = new Date(isoStr)
-  return date.toISOString() === isoStr
-}
+// Checks to see if its default JS iso date YYYY-MM-DDTHH:MN:SS.MSSZ
+export const isIsoString = (isoStr) => (/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(isoStr))
